af26c4c55de531adb0a05eed47f4111dd161ce85
William Welling <wwelling@library.tamu.edu>
2016-01-28 21:26:22 -0600

    Removed comment. Fully functional.

--- app/controllers/directiveGalleryController.js
+++ app/controllers/directiveGalleryController.js
@@ -1,7 +1,5 @@
 app.controller('DirectiveGalleryController', function ($scope, AlertService) {
 
-	//Removed Until functional
-
 	AlertService.add({type: "SUCCESS", message: "This is a success alert."}, "directive/gallery");
 	AlertService.add({type: "ERROR", message: "This is an error alert."}, "directive/gallery");
 	AlertService.add({type: "WARNING", message: "This is a warning alert."}, "directive/gallery");
