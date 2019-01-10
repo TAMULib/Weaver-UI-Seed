570af314c6ca67907cc1eca69b45390700a197e7
jeremythuff <jeremythuff@gmail.com>
2019-01-10 14:06:37 -0600

    Added controller barrel file

---
+++ app/controllers/index.js
@@ -0,0 +1,20 @@
+import AdminController from './adminController';
+import DirectiveGalleryController from './directiveGalleryController';
+import NavigationController from './navigationController';
+import ThemeController from './themeController';
+import UserRepoController from './userRepoController';
+
+ angular.module('app')
+    .controller('AdminController', AdminController)
+    .controller('DirectiveGalleryController', DirectiveGalleryController)
+    .controller('NavigationController', NavigationController)
+    .controller('ThemeController', ThemeController)
+    .controller('UserRepoController', UserRepoController)
+
+ export {
+  AdminController,
+  DirectiveGalleryController,
+  NavigationController,
+  ThemeController,
+  UserRepoController
+};
