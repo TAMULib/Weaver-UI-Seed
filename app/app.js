f77114bbb663da2967643e6a306e475fdb9ead94
jeremythuff <jeremythuff@gmail.com>
2019-01-10 14:26:15 -0600

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,49 +1,71 @@
-var app = angular.module('app', 
-[
-	'ngRoute',
-	'ngSanitize',
-	'app.version'
+import core from 'weaver-ui-core/app/core';
+import 'angular-messages';
+import 'angular-route';
+import 'angular-sanitize';
+import 'ng-table/bundles/ng-table';
+import 'angular-ui-bootstrap';
+import * as setup from 'weaver-ui-core/app/setup';
+import './resources/styles/sass/app.scss';
+
+
+var app = angular.module('app', [
+  'ngRoute',
+  'ngSanitize',
+  'ngTable'
 ]);
 
 app.model = core.model;
 app.repo = core.repo;
 
+var config = require('./config');
+require('./controllers');
+require('./model');
+// require('./directives');
+// require('./services');
+require('./repo');
+
 //This method's callback is passed to stomp and executed on both successfull connection, as well as disconnect.
-setUpApp(function(connected) {
+setup.setUpApp(config.appConfig, function (connected) {
+  //Indicates at app start if the app has successfully conenected to the service
+  config.appConfig.connected = connected;
 
-	//Indicates at app start if the app has successfully conenected to the service
-	appConfig.connected = connected;
+  app.constant('appConfig', config.appConfig);
+  app.constant('api', config.apiMapping);
+  angular.element(document).ready(function () {
+    var doc = angular.element(document);
+    var injector = doc.injector();
 
-	app.constant('appConfig', appConfig);
-	app.constant('api', apiMapping);
+    try {
+      //If the app is already bootstrapped then an error will be thrown
+      angular.bootstrap(document, ['app', 'core', 'ngMessages', 'ui.bootstrap']);
 
-	angular.element(document).ready(function() {
-		var doc = angular.element(document);
-		var injector = doc.injector();
-
-	   	try {
-	   		//If the app is already bootstrapped then an error will be thrown
-			angular.bootstrap(document, ['app', 'core', 'ui.bootstrap']);
-
-			if(!window.stompClient.connected) {
-				AlertService = injector.get('AlertService');
-	    		AlertService.add({type: "ERROR", message: "Web service cannot be reached."}, "/app/errors");
-			}	
-		} catch (e) {
-			console.log(e);
-			/*
-			 * If websockets dissconnect the app will attempt to re-bootstrap. Since the app is already running we will
-			 * end up in this block, and can generate an error indicating the disconnect.
-			*/
-			console.log("Angular could not bootstrap");
-	        if(typeof injector != 'undefined') {
-	        	AlertService = injector.get('AlertService');
-	    		AlertService.add({type: "ERROR", message: "Web service cannot be reached."}, "/app/errors");
-	        } else {
-	        	//foreward to error page
-	        	console.log('forward to error page');
-	        }
-	    }
-	});
-		
+      if (!window.stompClient.connected) {
+        var AlertService = injector.get('AlertService');
+        AlertService.add({
+          type: "ERROR",
+          message: "Web service cannot be reached."
+        }, "/app/errors");
+      }
+  } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e);
+      /*
+        * If websockets dissconnect the app will attempt to re-bootstrap. Since the app is already running we will
+        * end up in this block, and can generate an error indicating the disconnect.
+        */
+      // eslint-disable-next-line no-console
+      console.log("Angular could not bootstrap");
+      if (typeof injector != 'undefined') {
+        AlertService = injector.get('AlertService');
+        AlertService.add({
+          type: "ERROR",
+          message: "Web service cannot be reached."
+        }, "/app/errors");
+      } else {
+        //foreward to error page
+        // eslint-disable-next-line no-console
+        console.log('forward to error page');
+      }
+    }
+  });
 });
