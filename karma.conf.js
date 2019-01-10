a64d95e28e74f9dcb421167b86fcf79c078a2cac
jeremythuff <jeremythuff@gmail.com>
2019-01-10 13:34:11 -0600

    Updated karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,60 @@
 module.exports = function(config){
     config.set({
 
-        basePath : './',
+      preprocessors: {
+        "app/**/*.js": "coverage",
+        '**/*.html': ['ng-html2js'],
+        'test-context.js': ['webpack']
+      },
 
-        files : [
+      reporters: ['progress', 'coverage'],
 
-            'app/config/appConfig.js',
-            'app/config/apiMapping.js',
+      basePath : './',
 
-            'app/bower_components/jquery/dist/jquery.js',
-            'app/bower_components/angular/angular.js',
-            'app/bower_components/angular-mocks/angular-mocks.js',
-            'app/bower_components/angular-route/angular-route.js',
-            'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-            'app/bower_components/ng-table/dist/ng-table.min.js',
-            'app/bower_components/ng-csv/build/ng-csv.min.js',
-            'app/bower_components/angular-sanitize/angular-sanitize.min.js',
+      files : [
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',    
+        'dist/vendors~app.bundle.js',
+        'dist/app.bundle.js',
+        'node_modules/angular-mocks/angular-mocks.js',
+        'test-context.js',
+        'app/views/**/*.js',
+        'tests/mocks/**/*.js'
+      ],
 
-            'app/bower_components/core/app/config/coreConfig.js',
-
-            'app/bower_components/core/app/components/**/*.js',
-
-            'app/bower_components/core/app/core.js',
-
-            'app/bower_components/core/app/**/*.js',
-
-            
-            'app/components/**/*.js',
-
-            'tests/testSetup.js',
-            
-            'app/app.js',
-            
-            'app/config/runTime.js',
-            
-            'app/controllers/**/*.js',
-
-            'app/directives/**/*.js',
-            
-            'app/services/**/*.js',            
-            
-            'app/models/**/*.js',
-            
-            
-            'tests/mocks/**/*.js',
-
-            'tests/unit/**/*.js'
-            
-        ],
-
-        autoWatch : true,
-
-        frameworks: ['jasmine'],
-
-        browsers : ['Chrome'],
-
-        plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
-
-        junitReporter : {
-            outputFile: 'test_out/unit.xml',
-            suite: 'unit'
+      webpack: {
+        devtool: 'eval-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.js/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                }
+            ]
         }
+      },
+
+      autoWatch : true,
+
+      frameworks: ['jasmine'],
+
+      browsers : ['Chrome', 'Firefox'],
+
+      plugins : [
+        'karma-chrome-launcher',
+        'karma-coverage',
+        'karma-firefox-launcher',
+        'karma-jasmine',
+        'karma-junit-reporter',
+        'karma-ng-html2js-preprocessor',
+        'karma-webpack'
+      ],
+
+      coverageReporter: {
+        type: "lcov",
+        dir: "coverage/"
+      }
 
     });
 };
