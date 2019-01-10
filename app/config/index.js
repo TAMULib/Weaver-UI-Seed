66eb2075c1991a1b111703e8975f83dfbe1477ed
jeremythuff <jeremythuff@gmail.com>
2019-01-10 14:01:58 -0600

    Added barrel file for configuration

---
+++ app/config/index.js
@@ -0,0 +1,17 @@
+import { apiMapping } from './apiMapping';
+import { appConfig } from './appConfig';
+import routes from './routes';
+import runTime from './runTime';
+
+angular.module('app')
+    .constant('apiMapping', apiMapping)
+    .constant('appConfig', appConfig)
+    .config(routes)
+    .run(runTime);
+
+export {
+    apiMapping,
+    appConfig,
+    routes,
+    runTime
+};
