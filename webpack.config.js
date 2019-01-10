c2234b01f0927382b5aab60400abd3d5ab137094
jeremythuff <jeremythuff@gmail.com>
2019-01-10 16:25:05 -0600

    WIP html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,68 @@
 var path = require('path');
 var webpack = require('webpack');
 module.exports = {
-    context: __dirname + '/app',
-    mode: 'production',
-    entry: {
-        app: './app.js'
-    },
-    output: {
-        path: __dirname + '/app/dist',
-        filename: '[name].bundle.js'
-    },
-    devtool: 'eval-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                loader: 'eslint-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                include: [ path.resolve(__dirname, 'app') ],
-                exclude: [ path.resolve(__dirname, 'app/node_modules') ]
-            }
+  context: __dirname + '/app',
+  mode: 'production',
+  entry: {
+    app: './app.js'
+  },
+  output: {
+    path: __dirname + '/app/dist',
+    filename: '[name].bundle.js'
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
         ]
-    },
-    resolve: {
-        modules: [
-            path.resolve('./app'),
-            path.resolve('./node_modules'),
-            path.resolve('./node_modules/weaver-ui-core/')
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            "window.jQuery": 'jquery',
-            bootstrap: 'bootstrap'
-        })
-    ],
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
-
-    node: {
-        fs: 'empty'
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [path.resolve(__dirname, 'app')],
+        exclude: [path.resolve(__dirname, 'app/node_modules')]
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve('./app'),
+      path.resolve('./node_modules'),
+      path.resolve('./node_modules/weaver-ui-core/')
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      "window.jQuery": 'jquery',
+      bootstrap: 'bootstrap'
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
     }
+  },
+
+  node: {
+    fs: 'empty'
+  }
 };
